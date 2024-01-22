def0730ab95e0434baa71fa5ad5ccf8e990cdf20
quang nguyen <hquangng07@gmail.com>
2024-01-22 17:17:56 +0700

    edit file feature3/lesson3

--- Component/Lesson3/Lesson3/ViewController.swift
+++ Component/Lesson3/Lesson3/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var btClick: UIButton!
+    
+    // create label = code
+    var lbSecond = UILabel()
+    var btSecond = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupLabel()
+        setupSecondLabel()
+        setupButtonClick()
+        
     }
-
-
+    
+    func setupLabel() {
+        lbName.text = "Hello World with iOS!!!"
+        lbName.textColor = UIColor.brown
+        lbName.textAlignment = NSTextAlignment.left
+    }
+    
+    func setupSecondLabel() {
+        lbSecond.frame = CGRect(x: 20, y: 90, width: 200, height: 50)
+        view.addSubview(lbSecond)
+        
+        // set attri
+        lbSecond.backgroundColor = UIColor.black
+        lbSecond.textColor = .white
+        lbSecond.textAlignment = NSTextAlignment.center
+        lbSecond.text = "I'm iOS developer"
+        lbSecond.numberOfLines = 0
+        lbSecond.layer.cornerRadius = 8
+        lbSecond.layer.masksToBounds = true
+    }
+    
+    func setupButtonClick() {
+        btClick.setTitle("Tap me", for: .normal)
+        btClick.backgroundColor = .brown
+        btClick.setTitleColor(.white, for: .normal)
+        btClick.setImage(UIImage(), for: .normal)
+        btClick.layer.cornerRadius = 10
+        btClick.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        btClick.layer.masksToBounds = true
+    }
+        
+    @IBAction func tapOnClick(_ sender: Any) {
+        print("Thank you for tapping")
+        btClick.isSelected = !btClick.isSelected
+        if btClick.isSelected == true {
+            btClick.backgroundColor = .blue
+        } else {
+            btClick.backgroundColor = .red
+        }
+    }
+    
+    func setupSecondButton() {
+        
+    }
 }
 
