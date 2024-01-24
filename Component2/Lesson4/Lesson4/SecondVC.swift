a5cf881201900925ddb8674ecabde2d0c5c967d8
quang nguyen <hquangng07@gmail.com>
2024-01-24 13:15:24 +0700

    create and successful ui pickerview

--- Component2/Lesson4/Lesson4/SecondVC.swift
+++ Component2/Lesson4/Lesson4/SecondVC.swift
@@ -13,7 +13,12 @@ class SecondVC: UIViewController {
     @IBOutlet weak var lbResult: UILabel!
     @IBOutlet weak var aivLoading: UIActivityIndicatorView!
     @IBOutlet weak var progressView: UIProgressView!
-   
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    // Create array for picker view
+    var arr = ["male", "female"]
+    var list = ["iPhone", "iPad", "macbook", "apple watch", "mac mini"]
+    
     // Create time for progress view
     var timer = Timer()
     
@@ -23,7 +28,10 @@ class SecondVC: UIViewController {
     
     // Create segment = code
     var segmentedSecond = UISegmentedControl()
- 
+    
+    // Create picker = code
+    var secondPicker = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -31,6 +39,8 @@ class SecondVC: UIViewController {
         setupSegmentedSecond()
         setupProgressView()
         setupLoading()
+        setupPickerView()
+        setupSecondPicker()
     }
     
     func setupSegmentdControl() {
@@ -86,29 +96,88 @@ class SecondVC: UIViewController {
         }
     }
     
-    func setupLoading() {
-        aivLoading.hidesWhenStopped = true
-        aivLoading.color = .orange
-    }
-    
     func setupProgressView() {
         progressView.progressTintColor = .link
         progressView.trackTintColor = .systemGroupedBackground
         progressView.progress = 0.01
         
-        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(runProgress), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(runProgress), userInfo: nil, repeats: true)
     }
-    
+   
     @objc func runProgress() {
-        progressView.progress += 0.01
-        if Double(progressView.progress) == timer.timeInterval {
+        progressView.progress += 0.1
+        if Int(progressView.progress) == Int(timer.timeInterval) {
             aivLoading.stopAnimating()
-            aivLoading.isHidden = true
         }
     }
+    
+    func setupLoading() {
+        aivLoading.hidesWhenStopped = true
+        aivLoading.color = .orange
+    }
+    
+    func setupPickerView() {
+        pickerView.delegate = self
+        pickerView.dataSource = self
        
+    }
+    
+    func setupSecondPicker() {
+        let x = pickerView.frame.origin.x
+        let y = pickerView.frame.origin.y + pickerView.frame.height + 20
+        let w = pickerView.frame.width
+        let h = pickerView.frame.height
+        secondPicker.frame = CGRect(x: x, y: y, width: w, height: h)
+        view.addSubview(secondPicker)
+        
+        secondPicker.delegate = self
+        secondPicker.dataSource = self
+    }
+    
     @IBAction func moveView(_ sender: Any) {
         let vc  = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "webVC")
         navigationController?.pushViewController(vc, animated: true)
     }
 }
+
+extension SecondVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == self.pickerView {
+            return arr.count
+        } else {
+            return list.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == self.secondPicker {
+            return list[row]
+        } else {
+            return arr[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == self.secondPicker {
+            print(list[row])
+        } else {
+            print(arr[row])
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 20
+    }
+}
+
+// Show dữ liệu trên PickerView
+// 1. Khai báo Delegate + Dât Source
+// 2. Set Class kế thừa UIPickerViewDelegate, UIPickerViewDataSource
+// 3. Viết Extension
+// 4. Khai báo số cột + số dòng
+// 5. Khai báo nguồn dữ liệu hiển thị
+
