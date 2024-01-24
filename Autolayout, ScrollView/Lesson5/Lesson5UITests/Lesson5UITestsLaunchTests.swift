371169cafad0f5689d95f719c61b4b646e2169fb
quang nguyen <hquangng07@gmail.com>
2024-01-24 13:20:12 +0700

    create Lesson 5

---
+++ Autolayout, ScrollView/Lesson5/Lesson5UITests/Lesson5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lesson5UITestsLaunchTests.swift
+//  Lesson5UITests
+//
+//  Created by Quang Nguyen on 24/01/2024.
+//
+
+import XCTest
+
+final class Lesson5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
