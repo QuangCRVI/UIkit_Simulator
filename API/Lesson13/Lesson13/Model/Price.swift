eba87d15c6b2a8ffdd927e44829d1328443cb4b1
quang nguyen <hquangng07@gmail.com>
2024-02-05 11:37:06 +0700

    edit file

--- API/Lesson13/Lesson13/Model/Price.swift
+++ API/Lesson13/Lesson13/Model/Price.swift
@@ -1,27 +1,12 @@
 //
-//  Data.swift
+//  Price.swift
 //  Lesson13
 //
-//  Created by Quang Nguyen on 02/02/2024.
+//  Created by Quang Nguyen on 05/02/2024.
 //
 
 import Foundation
 import HandyJSON
-
-struct APIResponse: HandyJSON {
-    var count = 0
-    var entries = [Entries].self //Json Array
-}
-
-struct Entries: HandyJSON {
-    var API = ""
-    var Description = ""
-    var Auth = ""
-    var HTTPS = false
-    var Cors = ""
-    var Link = ""
-    var Category = ""
-}
 
 struct CoinDesk: HandyJSON {
     var time = Time() //Json Object
